16888705f75efb7c984a8b59f969bee49ceb9dda
achintyajha <achintyakjha@gmail.com>
2022-01-11 13:53:25 +0530

    Design implementation part 1 + logic improve

--- app.py
+++ app.py
@@ -2,26 +2,30 @@ from flask import Flask, request, render_template
 import random
 import ast
 
+# Setup
+
 with open('word-list.txt', 'r') as f:
     word_list = f.read().splitlines()
 
 last_word = dict()
 
 word = random.choice(word_list)
+chances = 5
 print(word)
 
 def restart():
     global word
     word = random.choice(word_list)
+    global chances
+    chances = 5
     print(word)
     open('user-words.txt', 'w').close()
 
-# Flask Stuff
+# Flask Stuff & Game Logic
 
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/model')
 def home():
     return render_template('index.html', result='opening', data=[])
 
@@ -34,6 +38,8 @@ def game():
     
     if len(user_word) == 5:
         if user_word in word_list:
+            global chances
+            chances -= 1
             if user_word == word:
                 result.append("You got that right!")
                 restart()
@@ -47,18 +53,20 @@ def game():
                             last_word[i] = [user_word[i], "green"]
                         else:
                             result.append(f"{user_word[i]} is not at the correct position")
-                            last_word[i] = [user_word[i], "yellow"]
+                            last_word[i] = [user_word[i], "goldenrod"]
                     else:
                         result.append(f"{user_word[i]} is not in the word")
                         last_word[i] = [user_word[i], "red"]
                         
         else:
             result.append("Invalid word")
+
     else:
         result.append("Only 5 letters words are allowed!")
 
     with open('user-words.txt', 'a') as f1:
-        f1.write(str(last_word)+'\n')
+        if last_word:
+            f1.write(str(last_word)+'\n')
      
     with open('user-words.txt', 'r') as f2:
         string = f2.read().splitlines()
@@ -67,7 +75,11 @@ def game():
         data.append(ast.literal_eval(line))
 
     print(data)
-    return render_template('index.html', result=result, data=data)
+    print(chances)
+    if data:
+        return render_template('index.html', result=result, data=data)
+    else:    
+        return render_template('index.html', result=result)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -86,5 +98,4 @@ def page_not_found(e):
 
 if __name__ == "__main__":
     app.run()
-
 
